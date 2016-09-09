9223ba2bfdb3ba27fcac470af67fc5cb4ccc7e01
Juca Crispim <juca@poraodojuca.net>
2016-09-09 20:16:08 -0300

    correcting build script upload2pypi

--- build-scripts/upload2pypi.sh
+++ build-scripts/upload2pypi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PYPACKAGE=`python setup.py sdist | egrep 'creating'|head -1 | sed 's/creating\s*//g'`;
-PYPACKAGE="$PYPACKAGE.tar.gz";
+PYPACKAGE="dist/$PYPACKAGE.tar.gz";
 echo "Uploading $PYPACKAGE to PyPI";
-twine upload $PYPACKAGE;
+twine upload "$PYPACKAGE";
