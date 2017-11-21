9f91ae3de86462bcc95afa9117c079b50ce8a263
Juca Crispim <juca@poraodojuca.net>
2017-11-21 12:59:56 -0200

    adding setup to doctools

--- doctools.py
+++ doctools.py
@@ -12,3 +12,12 @@ def source_role(typ, rawtext, text, lineno, inliner, options={}, content=[]):
     target = utils.unescape(target)
     refnode = nodes.reference(title, title, refuri=SOURCE_URI % target)
     return [refnode], []
+
+
+def setup(app):
+    """Install the plugin.
+
+    :param app: Sphinx application context.
+    """
+    app.add_role('source', source_role)
+    return
