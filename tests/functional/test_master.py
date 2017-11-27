b206f71f854f5e9603e98de129129e8261a27a3f
Juca Crispim <juca@poraodojuca.net>
2017-11-27 08:02:14 -0200

    typo

--- tests/functional/test_master.py
+++ tests/functional/test_master.py
@@ -157,7 +157,7 @@ class ToxicMasterTest(BaseFunctionalTest):
 
         with (yield from get_dummy_client()) as client:
             yield from client.wait_clone()
-            yield asyncio.sleep(1)
+            yield from asyncio.sleep(1)
 
         self.assertTrue(response)
 
