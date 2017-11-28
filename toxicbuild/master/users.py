f4991d0eef2c7d7583546fa765ad6a10b275f006
Juca Crispim <juca@poraodojuca.net>
2017-11-28 12:01:15 -0200

    correcting delete

--- toxicbuild/master/users.py
+++ toxicbuild/master/users.py
@@ -60,8 +60,8 @@ class User(Document):
     async def delete(self, *args, **kwargs):
         # this is here because we cant set CASCADE delete rule in
         # Organization, so we delete it here.
+        await super().delete(*args, **kwargs)
         await Organization.objects.filter(owner=self).delete()
-        super().delete(*args, **kwargs)
 
 
 class Team(EmbeddedDocument):
