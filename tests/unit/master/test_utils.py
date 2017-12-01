652eccbe82b848c0da28c863508e4f7fb82b7a4c
Juca Crispim <juca@poraodojuca.net>
2017-12-01 01:24:20 -0200

    adding master.utils

---
+++ tests/unit/master/test_utils.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Juca Crispim <juca@poraodojuca.net>
+
+# This file is part of toxicbuild.
+
+# toxicbuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# toxicbuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with toxicbuild. If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+from toxicbuild.master import utils, users
+from tests import async_test
+
+
+class UtilsTest(TestCase):
+
+    def test_as_db_ref(self):
+        user = users.User()
+        self.assertFalse(utils.as_db_ref(user, 'member_of'))
+
+
+class TestDoc(utils.OwnedDocument):
+    pass
+
+
+class OwnedDocuentTest(TestCase):
+
+    @async_test
+    async def setUp(self):
+        self.owner = users.User(username='zezinho@nada.co', password='123')
+        await self.owner.save()
+        self.doc = TestDoc(owner=self.owner)
+        await self.doc.save()
+
+    @async_test
+    async def test_get_for_user_owner(self):
+        doc = await TestDoc.get_for_user(
+            self.owner, id=self.doc.id)
+        self.assertEqual(doc, self.doc)
+
+    @async_test
+    async def test_get_for_user_denied(self):
+        user = users.User(username='b@b.com', password='123')
+        await user.save()
+        with self.assertRaises(utils.NotEnoughPerms):
+            await TestDoc.get_for_user(user, id=self.doc.id)
+
+    @async_test
+    async def test_get_for_user_organization(self):
+        user = users.User(username='b@b.com', password='123')
+        await user.save()
+        org = users.Organization(name='my-org', owner=user)
+        await org.save()
+        repo = TestDoc(owner=org)
+        await repo.save()
+        await user.reload()
+        returned = await TestDoc.get_for_user(user, id=repo.id)
+        self.assertEqual(repo, returned)
+
+    @async_test
+    async def test_list_for_user(self):
+        user = users.User(username='b@b.com', password='123')
+        await user.save()
+        org = users.Organization(name='my-org', owner=user)
+        await org.save()
+        repo = TestDoc(owner=org)
+        await repo.save()
+        repos = TestDoc.list_for_user(self.owner)
+        count = await repos.count()
+        self.assertEqual(count, 1)
+
+    @async_test
+    async def test_list_for_user_no_perms(self):
+        user = users.User(username='b@b.com', password='123')
+        await user.save()
+        repos = TestDoc.list_for_user(user)
+        count = await repos.count()
+        self.assertEqual(count, 0)
+
+    @async_test
+    async def test_list_for_user_organization(self):
+        user = users.User(username='b@b.com', password='123')
+        await user.save()
+        org = users.Organization(name='my-org', owner=user)
+        await org.save()
+        repo = TestDoc(owner=org)
+        await repo.save()
+        await user.reload()
+        repos = TestDoc.list_for_user(user)
+        count = await repos.count()
+        self.assertEqual(count, 1)
---
+++ toxicbuild/master/utils.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Juca Crispim <juca@poraodojuca.net>
+
+# This file is part of toxicbuild.
+
+# toxicbuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# toxicbuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with toxicbuild. If not, see <http://www.gnu.org/licenses/>.
+
+from bson.dbref import DBRef
+from mongoengine.queryset import Q
+from mongomotor import Document
+from mongomotor.fields import GenericReferenceField
+from toxicbuild.master.exceptions import NotEnoughPerms
+
+
+def as_db_ref(document, field):
+    """Returns reference field of a document as DBRefs."""
+
+    try:
+        dref = document._fields[field]._auto_dereference
+        document._fields[field]._auto_dereference = False
+        dbref = getattr(document, field)
+    finally:
+        document._fields[field]._auto_dereference = dref
+
+    return dbref
+
+
+class OwnedDocument(Document):
+
+    owner = GenericReferenceField(required=True)
+
+    meta = {'abstract': True}
+
+    @classmethod
+    async def get_for_user(cls, user, **kwargs):
+        """Returns a repository if ``user`` has permission for it.
+        If not raises an error.
+
+        :param user: User who is requesting the repository.
+        :param kwargs: kwargs to match the repository.
+        """
+        obj = await cls.objects.get(**kwargs)
+        has_perms = await obj._check_perms(user)
+        if not has_perms:
+            msg = 'The user {} has no permissions for this repository'.format(
+                user.id)
+            raise NotEnoughPerms(msg)
+        return obj
+
+    @classmethod
+    def list_for_user(cls, user, **kwargs):
+        """Returns a queryset of repositories in which the user has read
+        permission"""
+        member_of = [ref.id for ref in as_db_ref(user, 'member_of')]
+        organizations = [ref.id for ref in as_db_ref(user, 'organizations')]
+
+        qs = cls.objects(
+            Q(owner=user) |
+            Q(__raw__={'owner._ref.$id': {'$in': organizations}}) |
+            Q(__raw__={'owner._ref.$id': {'$in': member_of}}))
+
+        return qs.filter(**kwargs)
+
+    async def _check_perms(self, user):
+        owner = await self.owner
+        owner_id = DBRef(owner.__class__.__name__.lower(), owner.id)
+        member_of = as_db_ref(user, 'member_of')
+        organizations = as_db_ref(user, 'organizations')
+        if owner == user or owner_id in member_of or \
+           owner_id in organizations:
+            has_perms = True
+        else:
+            has_perms = False
+
+        return has_perms
