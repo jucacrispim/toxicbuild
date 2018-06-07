e9c55694859539f9326d90245b4db79e232f1043
Juca Crispim <juca@poraodojuca.net>
2018-06-07 00:59:05 -0300

    removing useless import

--- toxicbuild/slave/protocols.py
+++ toxicbuild/slave/protocols.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with toxicbuild. If not, see <http://www.gnu.org/licenses/>.
 
-import asyncio
 import traceback
 from toxicbuild.core.protocol import BaseToxicProtocol
 from toxicbuild.core.utils import log, datetime2string, now
