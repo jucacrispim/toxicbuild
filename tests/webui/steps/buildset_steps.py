ccd53e2ee9e322cee46dcdfe3150b0ebdb7eb496
Juca Crispim <juca@poraodojuca.net>
2019-05-28 06:16:55 -0300

    Pylint

--- tests/webui/steps/buildset_steps.py
+++ tests/webui/steps/buildset_steps.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import time
 from behave import when, then, given
 from selenium.common.exceptions import StaleElementReferenceException
 from tests.webui import take_screenshot
