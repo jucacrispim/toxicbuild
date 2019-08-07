d5c71ef2da9940f7a95e071273908751129a748a
Juca Crispim <juca@poraodojuca.net>
2019-08-07 17:57:49 -0300

    Fix clean images script

--- build-scripts/clean_images.sh
+++ build-scripts/clean_images.sh
@@ -7,8 +7,5 @@ docker rmi $(docker images --filter "before=toxicbase:latest"  -q)
 docker rmi $(docker images --filter "before=toxictest:latest"  -q)
 docker rmi $(docker images --filter "before=toxictest-selenium:latest"  -q)
 
-# delete old replicasets
-kubectl get rs | egrep '0\s+0\s+0' | cut -d' ' -f1 | xargs kubectl delete rs
-
 # always work
 exit 0
