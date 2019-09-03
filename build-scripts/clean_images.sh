0ce8af7f9768ab6f9229bb9cc1b8d6aef26138af
Juca Crispim <juca@poraodojuca.net>
2019-09-03 07:41:07 -0300

    Show output for clean images script

--- build-scripts/clean_images.sh
+++ build-scripts/clean_images.sh
@@ -3,9 +3,9 @@
 # clean old image to save disk space
 echo "Cleaning images"
 
-docker rmi $(docker images --filter "before=toxicbase:latest"  -q) &> /dev/null
-docker rmi $(docker images --filter "before=toxictest:latest"  -q) &> /dev/null
-docker rmi $(docker images --filter "before=toxictest-selenium:latest"  -q) &> /dev/null
+docker rmi $(docker images --filter "before=toxicbase"  -q) &> /dev/null
+docker rmi $(docker images --filter "before=toxictest"  -q) &> /dev/null
+docker rmi $(docker images --filter "before=toxictest-selenium"  -q) &> /dev/null
 
 # always work
 exit 0
