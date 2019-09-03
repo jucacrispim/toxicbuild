d01c6f1ccb64a24959bbec4215cea782c2eab3a7
Juca Crispim <juca@poraodojuca.net>
2019-09-03 08:26:20 -0300

    Fix clean_images script

--- build-scripts/clean_images.sh
+++ build-scripts/clean_images.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 # clean old image to save disk space
-echo "Cleaning images"
+echo "Cleaning environment..."
 
+echo "  Stopping containers..."
+docker stop $(docker ps -q)
+echo "  Done!"
+echo "\n"
+
+echo "  Removing containers..."
+docker rm $(docker ps -a -q)
+echo "  Done!"
+echo "\n"
+
+echo "  Removing old images..."
 docker rmi $(docker images --filter "before=toxicbase"  -q)
 docker rmi $(docker images --filter "before=toxictest"  -q)
 docker rmi $(docker images --filter "before=toxictest-selenium"  -q)
+echo "  Done!"
+echo "\n"
+
+echo "Environment clean!"
 
 
 # always work
