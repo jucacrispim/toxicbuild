a6e87edeb51f4fde47bbe9501d475b8ebead09b0
Juca Crispim <juca@poraodojuca.net>
2019-09-03 08:17:01 -0300

    Trigger build
    ci: include-builders python

--- build-scripts/clean_images.sh
+++ build-scripts/clean_images.sh
@@ -3,9 +3,10 @@
 # clean old image to save disk space
 echo "Cleaning images"
 
-docker rmi $(docker images --filter "before=toxicbase"  -q) &> /dev/null
-docker rmi $(docker images --filter "before=toxictest"  -q) &> /dev/null
-docker rmi $(docker images --filter "before=toxictest-selenium"  -q) &> /dev/null
+docker rmi $(docker images --filter "before=toxicbase"  -q)
+docker rmi $(docker images --filter "before=toxictest"  -q)
+docker rmi $(docker images --filter "before=toxictest-selenium"  -q)
+
 
 # always work
 exit 0
