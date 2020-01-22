5d6292656f640d0d8a73ebd421379c48fc4da2ec
Juca Crispim <juca@poraodojuca.net>
2020-01-22 01:04:56 -0300

    Add python docker images to build-images

--- build-scripts/toxicimages.sh
+++ build-scripts/toxicimages.sh
@@ -5,6 +5,11 @@ create_debian_generic(){
     docker tag toxic-debian-generic jucacrispim/toxiccontainers:debian-generic
 }
 
+create_debian_generic_docker(){
+    docker build -f build-images/debian/Dockerfile-debian-generic-docker -t toxic-debian-generic-docker build-images/debian
+    docker tag toxic-debian-generic-docker jucacrispim/toxiccontainers:debian-generic-docker
+}
+
 create_debian_python35(){
     docker build -f build-images/debian/Dockerfile-debian-python3.5 -t toxic-debian-python35 build-images/debian
     docker tag toxic-debian-python35 jucacrispim/toxiccontainers:debian-python3.5
@@ -20,20 +25,43 @@ create_debian_python37(){
     docker tag toxic-debian-python37 jucacrispim/toxiccontainers:debian-python3.7
 }
 
+create_debian_python35_docker(){
+    docker build -f build-images/debian/Dockerfile-debian-python3.5-docker -t toxic-debian-python35-docker build-images/debian
+    docker tag toxic-debian-python35-docker jucacrispim/toxiccontainers:debian-python3.5-docker
+}
+
+create_debian_python36_docker(){
+    docker build -f build-images/debian/Dockerfile-debian-python3.6-docker -t toxic-debian-python36-docker build-images/debian
+    docker tag toxic-debian-python36-docker jucacrispim/toxiccontainers:debian-python3.6-docker
+}
+
+create_debian_python37_docker(){
+    docker build -f build-images/debian/Dockerfile-debian-python3.7-docker -t toxic-debian-python37-docker build-images/debian
+    docker tag toxic-debian-python37-docker jucacrispim/toxiccontainers:debian-python3.7-docker
+}
+
 
 create_images(){
     create_debian_generic
+    create_debian_generic_docker
     create_debian_python37
+    create_debian_python37_docker
     create_debian_python36
+    create_debian_python36_docker
     create_debian_python35
+    create_debian_python35_docker
 }
 
 
 upload_images(){
     docker push jucacrispim/toxiccontainers:debian-generic
+    docker push jucacrispim/toxiccontainers:debian-generic-docker
     docker push jucacrispim/toxiccontainers:debian-python3.7
+    docker push jucacrispim/toxiccontainers:debian-python3.7-docker
     docker push jucacrispim/toxiccontainers:debian-python3.6
+    docker push jucacrispim/toxiccontainers:debian-python3.6-docker
     docker push jucacrispim/toxiccontainers:debian-python3.5
+    docker push jucacrispim/toxiccontainers:debian-python3.5-docker
 }
 case "$1" in
 
