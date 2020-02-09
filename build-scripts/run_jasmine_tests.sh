3a5cbefea3bb164a24327b550e5984edb9ae262f
Juca Crispim <juca@poraodojuca.net>
2020-02-09 19:03:51 -0300

    Fix js unit tests on ci
    ci: inlclude-builders selenium

--- build-scripts/run_jasmine_tests.sh
+++ build-scripts/run_jasmine_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export DISPLAY=:99
-Xvfb :99  -ac -screen 0, 1368x768x24 &
-toxicjasmine ci -c tests/js-unit/jasmine.yml --browser chrome --options=no-sandbox
+ Xvfb :99  -ac -screen 0, 1368x768x24 &
+toxicjasmine ci -c tests/js-unit/jasmine.yml --browser chrome --options=no-sandbox,disable-dev-shm-usage,headless,remote-debugging-port=9222,disable-gpu
 status=$?
-killall Xvfb
-rm -f /tmp/.X99-lock
+ killall Xvfb
+ rm -f /tmp/.X99-lock
 exit $status
