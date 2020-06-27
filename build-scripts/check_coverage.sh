a11a26740838aa611d17ba1550262d0ac5bfe150
Juca Crispim <juca@poraodojuca.net>
2020-06-27 19:00:04 -0300

    change check_coverage script

--- build-scripts/check_coverage.sh
+++ build-scripts/check_coverage.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 echo "\nChecking coverage for Python code\n"
-OUT=`coverage run --source=$1 --branch --omit=toxicbuild/script.py,toxicbuild/master/__init__.py,toxicbuild/slave/__init__.py,toxicbuild/ui/__init__.py,toxicbuild/integrations/__init__.py,toxicbuild/output/__init__.py,toxicbuild/script.py,toxicbuild/poller/__init__.py setup.py test --test-suite=tests.unit`;
+OUT=`coverage run --source=toxicbuild --branch --omit=toxicbuild/script.py,toxicbuild/master/__init__.py,toxicbuild/slave/__init__.py,toxicbuild/ui/__init__.py,toxicbuild/integrations/__init__.py,toxicbuild/output/__init__.py,toxicbuild/script.py,toxicbuild/poller/__init__.py setup.py test --test-suite=tests.unit`;
 ERROR=$?
+cov_threshold=100
 coverage html;
 coverage=`coverage report -m | grep TOTAL | sed 's/TOTAL\s*\w*\s*\w*\s*\w*\s*\w*//g' | cut -d'%' -f1`
 
@@ -10,7 +11,7 @@ echo 'coverage was:' $coverage'%'
 
 if [ "$ERROR" != "0" ]
 then
-    if [ $coverage -eq $2 ]
+    if [ $coverage -eq "$cov_threshold" ]
     then
 	echo "But something went wrong";
 	echo "$OUT";
@@ -22,7 +23,7 @@ then
     fi
 fi
 
-if [ $coverage -eq $2 ]
+if [ $coverage -eq "$cov_threshold" ]
 then
     echo "Yay! Everything ok!";
     exit 0;
